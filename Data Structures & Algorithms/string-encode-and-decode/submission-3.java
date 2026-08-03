e75e985902bae83de1c568a6c9ce0d85ad7de9c3
Nelvin Joseph <nelvinjoseph1@gmail.com>
2026-08-03 17:58:46 +0100

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String encode(List<String> strs) {
+        StringJoiner strJoiner = new StringJoiner("");
+        
+        strs.forEach(str -> strJoiner.add(str.length()+"#"+str));
+
+        return strJoiner.toString();
+    }
+
+    public List<String> decode(String str) {
+        System.out.println(str);
+        List<String> result = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            int j = str.indexOf('#', i);
+            int lengthOfString = Integer.parseInt(str.substring(i, j));
+            result.add(str.substring(j + 1, j + 1 + lengthOfString));
+
+            i = j + 1 + lengthOfString;
+        }
+
+        return result;
+    }
+}
